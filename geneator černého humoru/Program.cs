81a9bcb46329bfb5d4295da1e2cc5b65da7523c9
Artur Ostrý <a.ostry@gymjs.cz>
2025-05-15 15:36:14 +0200

    Přidejte soubory projektu.

---
+++ geneator černého humoru/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Zadej libovoný počet vtipných textíků: ");
+string pocetText = Console.ReadLine();
+int pocetVtipu = Convert.ToInt32(pocetText);
+
+Console.WriteLine(pocetVtipu * 3);
