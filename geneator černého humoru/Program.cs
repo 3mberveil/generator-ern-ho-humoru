9ba7046535e14fd006187bb9368991e77b58afbd
3mberveil <a.ostry@gymjs.cz>
2025-06-18 15:27:52 +0200

    update

--- geneator černého humoru/Program.cs
+++ geneator černého humoru/Program.cs
@@ -1,5 +1,26 @@
-﻿Console.WriteLine("Zadej libovoný počet vtipných textíků: ");
+﻿Console.WriteLine("Zadej počet vtipů pro vygenerování: ");
 string pocetText = Console.ReadLine();
 int pocetVtipu = Convert.ToInt32(pocetText);
 
-Console.WriteLine(pocetVtipu * 3);
+var client = new System.Net.Http.HttpClient();
+
+int i = 0;
+while (i < pocetVtipu)
+{
+    var response = client.GetAsync("https://geek-jokes.sameerkumar.website/api?format=text").Result;
+    if (response.IsSuccessStatusCode)
+    {
+        string vtip = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(vtip);
+    }
+    else
+    {
+        Console.WriteLine("Nepodařilo se načíst vtip.");
+    }
+
+    Console.WriteLine("-----------------------------------------------");
+    System.Threading.Thread.Sleep(2000); // pauza 2 sekundy
+    i++;
+}
+
+Console.WriteLine("DĚKUJEME ZA VYUŽITÍ GENERÁTORU! :)");
